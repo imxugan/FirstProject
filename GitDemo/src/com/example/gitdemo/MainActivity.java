e3aaad1c94d234faed609ac1b7fd49a62ddb572d
xugan <xugan@xywy.com>
2015-10-09 17:09:11 +0800

    添加了Notification的使用方式

--- GitDemo/src/com/example/gitdemo/MainActivity.java
+++ GitDemo/src/com/example/gitdemo/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.gitdemo;
 
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
@@ -11,11 +10,16 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat.Builder;
 import android.support.v4.content.LocalBroadcastManager;
 import android.text.TextUtils;
 import android.view.Menu;
@@ -36,6 +40,8 @@ public class MainActivity extends Activity {
 	private LocalBroadcastManager manager;
 	private MyLoginBroadCastReceiver receiver;
 	private EditText et_input;
+	private Builder mBuilder;
+	private NotificationManager service;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -49,6 +55,8 @@ public class MainActivity extends Activity {
 		
 		forceOffLine();
 		
+		
+		
 	}
 	
 	private void init() {
@@ -60,6 +68,27 @@ public class MainActivity extends Activity {
 			et_input.setSelection(data.length());
 		}
 		
+		service = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		
+	}
+	
+	/** 初始化通知栏 */
+	private void initNotify(){
+		mBuilder = new NotificationCompat.Builder(this);
+		mBuilder.setContentTitle("测试标题")
+				.setContentText("测试内容")
+//				.setContentIntent(getDefalutIntent(Notification.FLAG_AUTO_CANCEL))
+//				.setNumber(number)//显示数量
+				.setTicker("测试通知来啦")//通知首次出现在通知栏，带上升动画效果的
+				.setWhen(System.currentTimeMillis())//通知产生的时间，会在通知信息里显示
+				.setPriority(Notification.PRIORITY_DEFAULT)//设置该通知优先级
+				.setAutoCancel(true)
+				.setOngoing(false)//ture，设置他为一个正在进行的通知。他们通常是用来表示一个后台任务,用户积极参与(如播放音乐)或以某种方式正在等待,因此占用设备(如一个文件下载,同步操作,主动网络连接)
+				.setDefaults(Notification.DEFAULT_VIBRATE)//向通知添加声音、闪灯和振动效果的最简单、最一致的方式是使用当前的用户默认设置，使用defaults属性，可以组合：
+				//Notification.DEFAULT_ALL  Notification.DEFAULT_SOUND 添加声音 // requires VIBRATE permission
+				.setSmallIcon(R.drawable.ic_launcher);
+		service.notify(1, mBuilder.build());
+		service.cancel(1);//添加这句代码，通知会在状态栏自动消失
 	}
 
 	private String readSavedData() {
@@ -190,12 +219,50 @@ public class MainActivity extends Activity {
 			break;
 		case R.id.btn_test_database:
 			startActivity(new Intent(MainActivity.this,DataBaseActivity.class));	
-
+			break;
+		case R.id.btn_notification:
+			initNotify();
+			break;
+		case R.id.btn_notify_anthor_activity:
+			notifyActivity();
 		default:
 			break;
 		}
 	}
 	
+	/** 初始化通知栏 */
+	private void notifyActivity(){
+		mBuilder = new NotificationCompat.Builder(this);
+		mBuilder.setContentTitle("测试标题")
+//				.setLights(Color.RED, 1000, 1000)//需要<uses-permission android:name="android.permission.FLASHLIGHT">
+				.setContentText("打开一个新的activity")
+				.setContentIntent(getDefalutIntent(Notification.FLAG_AUTO_CANCEL))
+//				.setNumber(number)//显示数量
+				.setTicker("测试通知来啦")//通知首次出现在通知栏，带上升动画效果的
+				.setWhen(System.currentTimeMillis())//通知产生的时间，会在通知信息里显示
+				.setPriority(Notification.PRIORITY_DEFAULT)//设置该通知优先级
+				.setAutoCancel(true)//设置??????
+				.setOngoing(false)//ture，设置他为一个正在进行的通知。他们通常是用来表示一个后台任务,用户积极参与(如播放音乐)或以某种方式正在等待,因此占用设备(如一个文件下载,同步操作,主动网络连接)
+				.setDefaults(Notification.DEFAULT_VIBRATE|Notification.DEFAULT_LIGHTS)//向通知添加声音、闪灯和振动效果的最简单、最一致的方式是使用当前的用户默认设置，使用defaults属性，可以组合：
+				//Notification.DEFAULT_ALL  Notification.DEFAULT_SOUND 添加声音 // requires VIBRATE permission
+				.setSmallIcon(R.drawable.ic_launcher);
+		Notification build = mBuilder.build();
+		service.notify(2, build);
+//		通过查看灯的实现源码，发现如下问题： 
+//		在来电或者系统屏幕亮的情况下是没办法控制灯的
+	}
+	
+	/**
+	 * @获取默认的pendingIntent,为了防止2.3及以下版本报错
+	 * @flags属性:  
+	 * 在顶部常驻:Notification.FLAG_ONGOING_EVENT  
+	 * 点击去除： Notification.FLAG_AUTO_CANCEL 
+	 */
+	public PendingIntent getDefalutIntent(int flags){
+		PendingIntent pendingIntent= PendingIntent.getActivity(this, 1, new Intent(this,NotificationActivity.class), flags);
+		return pendingIntent;
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
